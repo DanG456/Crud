369d71354bebff2be572bee83470d10102a3d906
DanG456 <daniel.gil1922@gmail.com>
2022-07-06 14:32:21 -0500

    Files Creation
    Creación de archivos necesarios y modificacion de index.php

---
+++ php_files/index.php
@@ -0,0 +1,49 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.3/font/bootstrap-icons.css">
+    
+    <!-- Data tables -->
+    <link href="https://cdn.datatables.net/1.12.1/css/jquery.dataTables.min.css" rel="stylesheet">
+    <link href="https://cdn.datatables.net/1.12.1/js/jquery.dataTables.min.js" rel="stylesheet">
+
+    <!-- Estilos Css-->
+    <link rel="stylesheet" href="../css/styles.css">
+
+    <title>Crud con PHP y Ajax</title>
+  </head>
+  <body>
+    <div class="container">
+        <h1 class="text-center">CRUD con PHP y Ajax</h1>
+        <div class="row">
+            <div class="col-2 offset-10">
+                <div class="text-center">
+                    <!-- Button trigger modal -->
+                    <button type="button" class="btn btn-primary w-100" data-bs-toggle="modal" data-bs-target="#userModal"> 
+                        <i class="bi bi-plus-circle"></i> Crear
+                    </button>
+                </div>
+            </div>
+        </div>
+        
+        <br>
+        <br>
+
+        <div class="table-responsive">
+
+        </div>
+
+    </div>
+
+    <!-- Option 1: Bootstrap Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+    <script src="https://code.jquery.com/jquery-3.6.0.js" integrity="sha256-H+K7U5CnXl1h5ywQfKtSj8PCmoN9aaq30gDh27Xc0jk=" crossorigin="anonymous"></script>
+
+  </body>
+</html>
